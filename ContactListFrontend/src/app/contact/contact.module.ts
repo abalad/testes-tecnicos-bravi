e2791c11ff4c8ba1379163146824be31ae77cb0e
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2018-09-19 19:43:08 -0300

    criar modulo contact

---
+++ ContactListFrontend/src/app/contact/contact.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { ContactComponent } from './containers/contact/contact.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  declarations: [
+    ContactComponent
+  ],
+  exports: [
+    ContactComponent
+  ]
+})
+export class ContactModule { }
--- ContactListFrontend/src/app/main/app.module.ts
+++ ContactListFrontend/src/app/main/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from '../shared/shared.module';
+import { ContactComponent } from '../contact/containers/contact/contact.component';
+import { ContactModule } from '../contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,11 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    ContactModule,
+    RouterModule.forRoot([
+        { path: '', redirectTo: 'main', pathMatch: 'full' },
+        { path: 'main', component: ContactComponent }
+    ]),
     SharedModule,
   ],
   providers: [],
